c6369ba39be02d26f91071fb93a876c6387b67fb
Ariel  Molina <itzli2000@msn.com>
2021-02-03 14:52:58 -0600

    Refactor: created constants file for canvas component

--- src/components/Canvas/Canvas.tsx
+++ src/components/Canvas/Canvas.tsx
@@ -1,5 +1,8 @@
 import React, { useEffect } from 'react';
 import * as THREE from 'three';
+import {
+  ASPECT, DEFAULT_MESH_COLOR, FAR, FOV, NEAR,
+} from '../../constants/three-config';
 import './Canvas.css';
 
 const Canvas = () => {
@@ -9,17 +12,12 @@ const Canvas = () => {
 
   const init = () => {
     const scene = new THREE.Scene();
-    const camera = new THREE.PerspectiveCamera(
-      75,
-      window.innerWidth / window.innerHeight,
-      0.1,
-      1000,
-    );
+    const camera = new THREE.PerspectiveCamera(FOV, ASPECT, NEAR, FAR);
     const renderer = new THREE.WebGLRenderer();
     renderer.setSize(window.innerWidth, window.innerHeight);
     document.body.appendChild(renderer.domElement);
     const geometry = new THREE.BoxGeometry(1, 1, 1);
-    const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+    const material = new THREE.MeshBasicMaterial({ color: DEFAULT_MESH_COLOR });
     const cube = new THREE.Mesh(geometry, material);
     scene.add(cube);
     camera.position.z = 5;
---
+++ src/constants/three-config.tsx
@@ -0,0 +1,5 @@
+export const FOV = 75;
+export const ASPECT = window.innerWidth / window.innerHeight;
+export const NEAR = 0.1;
+export const FAR = 1000;
+export const DEFAULT_MESH_COLOR = 0x0000FF;
